8b42d079a498e3810dcc64aef85a20034616944f
aanghel90 <adina.anghel90@gmail.com>
2025-07-06 20:27:28 +0300

    Am refactorizat WindowTest.

--- src/test/java/tests/WindowsTest.java
+++ src/test/java/tests/WindowsTest.java
@@ -1,5 +1,6 @@
 package tests;
 
+import helperMethods.ElementHelper;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
@@ -16,16 +17,17 @@ public class WindowsTest extends SharedData {
     @Test
     public void testMethod() {
 
-        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        ElementHelper elementHelper = new ElementHelper(driver);
+
 
         WebElement elementAlertsFrameWindows = driver.findElement(By.xpath("//h5[text()='Alerts, Frame & Windows']"));
-        executor.executeScript("arguments[0].click();", elementAlertsFrameWindows);
+        elementHelper.ckickJSElement(elementAlertsFrameWindows);
 
         WebElement elementBrowserWindows = driver.findElement(By.xpath("//span[text()='Browser Windows']"));
-        executor.executeScript("arguments[0].click();", elementBrowserWindows);
+        elementHelper.ckickJSElement(elementBrowserWindows);
 
         WebElement newTabElement = driver.findElement(By.id("tabButton"));
-        newTabElement.click();
+        elementHelper.clickElement(newTabElement);
 
         System.out.println(driver.getCurrentUrl());
 
@@ -41,7 +43,7 @@ public class WindowsTest extends SharedData {
 
 
         WebElement newWindowElement = driver.findElement(By.id("windowButton"));
-        newWindowElement.click();
+        elementHelper.clickElement(newWindowElement);
 
         System.out.println(driver.getCurrentUrl());
 
@@ -57,10 +59,6 @@ public class WindowsTest extends SharedData {
 
         //din cauza reclamei fortam site-ul sa acceseze un URL specific => dar sa nu facem asta, nu este recomandat
         //driver.navigate().to("https://demoqa.com/browser-windows");
-
-
-        //inchidem browserul cu toate taburile
-        driver.quit();
 
 
     }
